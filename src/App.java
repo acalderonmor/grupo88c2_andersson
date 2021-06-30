28d95a04ebd88ae4aeb92998ff6c0768763408c9
acalderonmor <andersson.calderon.m@gmail.com>
2021-06-30 11:35:56 -0500

    nuevo

--- src/App.java
+++ src/App.java
@@ -3,8 +3,9 @@ public class App {
 
     public static void main(String[] args) {
         boolean respuesta = 12 < 5 || 4<5;
+        int dato =2;
 
-        System.out.println(respuesta );  
+        System.out.println(dato );  
         
            
           
