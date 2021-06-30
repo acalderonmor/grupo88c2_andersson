84b37d5309517cdc8cf2c5bb50e6f131519c7b5e
acalderonmor <andersson.calderon.m@gmail.com>
2021-06-30 12:15:29 -0500

    contador

--- src/App.java
+++ src/App.java
@@ -2,16 +2,10 @@ public class App {
     
 
     public static void main(String[] args) {
-        int MAX_I =20 , MAX_J=10 ;
-        loops:
-        for (int i = 0; i < MAX_I; i++) {
-            System.out.print(MAX_I);
-            for (int j = 0; j < MAX_J; j++) {
-                
-                break loops;
-            }
-        }
-           
-          
+        int counter = 0;
+		while (counter < 5) {
+			counter ++;
+			System.out.println(counter);
+		}
     }
 }
