05bef44f0a1aa29ec50554f8ce0beaf13af5a509
acalderonmor <andersson.calderon.m@gmail.com>
2021-06-30 11:48:57 -0500

    nuevo

--- src/App.java
+++ src/App.java
@@ -2,11 +2,15 @@ public class App {
     
 
     public static void main(String[] args) {
-        boolean respuesta = 12 < 5 || 4<5;
-        int dato =2;
-
-        System.out.println(dato );  
-        
+        int MAX_I =20 , MAX_J=10 ;
+        loops:
+        for (int i = 0; i < MAX_I; i++) {
+            System.out.print(MAX_I);
+            for (int j = 0; j < MAX_J; j++) {
+                
+                break loops;
+            }
+        }
            
           
     }
