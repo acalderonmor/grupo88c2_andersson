0be54b38df4bd25bea1030f284ade53070cba8d8
acalderonmor <andersson.calderon.m@gmail.com>
2021-06-30 11:04:37 -0500

    ver

--- src/App.java
+++ src/App.java
@@ -1,6 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+    
+
+    public static void main(String[] args) {
+        boolean respuesta = 12 < 5 || 4<5;
+
+        System.out.println(respuesta );  
+        
+           
+          
     }
 }
